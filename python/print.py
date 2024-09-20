a5d7722691ff9e4b23bb55c7cda3bc10a801d88c
Sagar <shettysagar272@gmail.com>
2024-09-21 03:11:20 +0530

    understanding print function

---
+++ python/print.py
@@ -0,0 +1,6 @@
+print("Hello Python")
+#passing multiple argument to a python
+print("Hello" , "Python" , "!" )
+# end is use to add anything at the end of the the argument and sep is use to seprate the argumrnts with anything 
+# Below we have used new line which is \n it can be anything like '#,$,%'
+print("Hello to a new world of python", "Lock in time" , end=":)" , sep="\n")
